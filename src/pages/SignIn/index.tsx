124d4e69822a405fbae3d413866f09d78f8fa34e
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-28 14:56:57 -0300

    Add forgot link

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -24,6 +24,14 @@ const SignIn: React.FC = () => {
       >
         Entrar
       </Button>
+
+      <ForgotPassword
+        onPress={() => {
+          console.log('asd');
+        }}
+      >
+        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+      </ForgotPassword>
     </Container>
   );
 };
